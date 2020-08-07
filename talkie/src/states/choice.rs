16aa5b100d4ddeffcbfb452474a8e71245d113ea
Owen Nelson <onelson@gmail.com>
2020-08-06 23:35:14 -0700

    Add cursor position tracking on ChoiceState. (#23)
    * Add cursor position tracking on ChoiceState.
    
    This diff adds action trackers to ChoiceState and uses them to drive a
    cursor representing the selected choice. This will ultimately replace
    the mouse event code currently in use to trigger state transitions, but
    we're not quite there yet.
    
    In addition to adding the trackers, this also refactors aspects of the
    button creation such that we keep a separate vec of the y offsets that
    correspond to each choice, and therefore each button. I suspect
    there will be a refactor in the future to reduce the overall number of
    vecs owned by this struct. For now, it is what it is.
    
    Still todo:
    
    - revising the transition code to listen to the confirm action
      (and not the mouse clicks).
    - adding a graphic for the cursor position.
    
    * Add a cursor graphic to billboard ui. Drive it with cursor_pos.
    
    Was hoping to not split the graphics into their own files, but was
    unable to figure out how to load a spritesheet-backed image in the ui
    RON file. Oh well.

--- talkie/src/states/choice.rs
+++ talkie/src/states/choice.rs
@@ -1,9 +1,11 @@
 use crate::assets::Choice;
+use crate::components::ActionTracker;
 use amethyst::assets::Loader;
-use amethyst::core::ecs::{Builder, Entity, WorldExt};
-use amethyst::input::StringBindings;
-use amethyst::ui::{Anchor, FontHandle, Interactable, TtfFormat, UiEventType, UiText, UiTransform};
-use amethyst::{GameData, SimpleState, SimpleTrans, StateData, StateEvent, Trans};
+use amethyst::core::ecs::{Builder, Entity, World, WorldExt};
+use amethyst::core::HiddenPropagate;
+use amethyst::input::{InputHandler, StringBindings};
+use amethyst::ui::{Anchor, FontHandle, TtfFormat, UiFinder, UiText, UiTransform};
+use amethyst::{GameData, SimpleState, SimpleTrans, StateData, Trans};
 
 /// A `Resource` to track the outcomes of `ChoiceState`s.
 ///
@@ -19,11 +21,26 @@ pub struct ChoiceState {
     choices: Vec<Choice>,
     buttons: Vec<Entity>,
     font: Option<FontHandle>,
+    // y offsets for each of the buttons.
+    ys: Vec<f32>,
+    cursor_pos: usize,
+    cursor_gfx: Option<Entity>,
+
+    // trackers
+    confirm: ActionTracker,
+    up: ActionTracker,
+    down: ActionTracker,
 }
+
+const GUTTER_V: f32 = 4.;
+const BTN_HEIGHT: f32 = 32.;
+const BTN_WIDTH: f32 = 100.;
 
 impl SimpleState for ChoiceState {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
+        self.poll_inputs(world);
+
         self.font = Some(world.read_resource::<Loader>().load(
             "font/CC Accidenz Commons-medium.ttf",
             TtfFormat,
@@ -35,24 +52,13 @@ impl SimpleState for ChoiceState {
         let font = self.font.clone().unwrap();
 
         self.buttons = self
-            .choices
+            .ys
             .iter()
-            // rev to make it so that the idx is lower for the later choices
-            // Since y goes "up" based on the bottom left anchors, we want the
-            // earlier items to have higher values than the later ones for the
-            // purpose of generating y offsets.
-            .rev()
-            .enumerate()
-            .map(move |(idx, choice)| {
+            .zip(self.choices.iter())
+            .map(move |(&y, choice)| {
                 let mut ui_text =
                     UiText::new(font.clone(), choice.label.clone(), [0., 0., 0., 1.], 20.0);
                 ui_text.align = Anchor::MiddleLeft;
-
-                const GUTTER_V: f32 = 4.;
-                const BTN_HEIGHT: f32 = 30.;
-                const BTN_WIDTH: f32 = 100.;
-                // Mainly used to make room for the billboard bezel.
-                const PADDING: f32 = 30.;
 
                 world
                     .create_entity()
@@ -60,56 +66,104 @@ impl SimpleState for ChoiceState {
                         choice.label.clone(),
                         Anchor::BottomLeft,
                         Anchor::BottomLeft,
-                        PADDING,
-                        (idx as f32 * (BTN_HEIGHT + GUTTER_V)) + PADDING,
+                        60.,
+                        y,
                         3., // z index
                         BTN_WIDTH,
                         BTN_HEIGHT,
                     ))
                     .with(ui_text)
-                    .with(Interactable)
                     .build()
             })
-            // rev again so that the button index lines up with the choice index.
-            .rev()
             .collect();
     }
 
     fn on_stop(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         data.world.delete_entities(&self.buttons).unwrap();
         self.buttons.clear();
+
+        if let Some(gfx) = self.cursor_gfx {
+            let mut storage = data.world.write_storage::<HiddenPropagate>();
+            let _ = storage.insert(gfx, HiddenPropagate::new());
+        }
     }
 
-    fn handle_event(
-        &mut self,
-        data: StateData<'_, GameData<'_, '_>>,
-        event: StateEvent<StringBindings>,
-    ) -> SimpleTrans {
-        if let StateEvent::Ui(ui_event) = event {
-            match ui_event.event_type {
-                UiEventType::Click => {
-                    if let Some(btn_idx) = self.buttons.iter().position(|e| e == &ui_event.target) {
-                        let mut goto = data.world.try_fetch_mut::<Goto>().unwrap();
-                        goto.passage_group_id = self.choices[btn_idx].goto.clone();
-                        return Trans::Pop;
-                    }
-                }
-                _ => {
-                    return SimpleTrans::None;
-                }
-            };
+    fn fixed_update(&mut self, data: StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        if self.confirm.press_begin() {
+            log::debug!(
+                "cursor => {} {:?}",
+                self.cursor_pos,
+                self.choices[self.cursor_pos]
+            );
+            let mut goto = data.world.try_fetch_mut::<Goto>().unwrap();
+            goto.passage_group_id = self.choices[self.cursor_pos].goto.clone();
+            return Trans::Pop;
         }
         SimpleTrans::None
+    }
+
+    fn shadow_update(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        if self.cursor_gfx.is_none() {
+            world.exec(|ui_finder: UiFinder| {
+                self.cursor_gfx = ui_finder.find("cursor");
+            });
+        }
+
+        if let Some(gfx) = self.cursor_gfx {
+            let mut storage = world.write_storage::<HiddenPropagate>();
+            let _ = storage.remove(gfx);
+
+            let mut storage = world.write_storage::<UiTransform>();
+            let xform = storage.get_mut(gfx).unwrap();
+            xform.local_y = self.ys[self.cursor_pos];
+        }
+
+        self.poll_inputs(world);
     }
 }
 
 impl ChoiceState {
     pub fn new(choices: Vec<Choice>) -> ChoiceState {
-        let buttons = Vec::with_capacity(choices.len());
+        let count = choices.len();
+        let buttons = Vec::with_capacity(count);
         ChoiceState {
             choices,
+            cursor_pos: 0,
+            cursor_gfx: None,
             buttons,
             font: None,
+            ys: (0..count)
+                .map(|idx| (count - idx) as f32 * (BTN_HEIGHT + GUTTER_V))
+                .collect(),
+
+            confirm: ActionTracker::new("confirm"),
+            up: ActionTracker::new("up"),
+            down: ActionTracker::new("down"),
+        }
+    }
+
+    fn poll_inputs(&mut self, world: &mut World) {
+        let input = world.read_resource::<InputHandler<StringBindings>>();
+        self.confirm.update(&input);
+        self.down.update(&input);
+        self.up.update(&input);
+
+        if self.up.press_begin() {
+            self.cursor_pos = if self.cursor_pos > 0 {
+                self.cursor_pos - 1
+            } else {
+                0
+            };
+        } else if self.down.press_begin() {
+            // choices better me non-empty...
+            let max = self.choices.len() - 1;
+            self.cursor_pos = if self.cursor_pos < max {
+                self.cursor_pos + 1
+            } else {
+                max
+            };
         }
     }
 }
